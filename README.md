03a34dc7f3a2ee448abed01ea82a548291c6fba3
Jeremy <jjdesnoyer@gmail.com>
2023-06-23 19:04:09 +0200

    Add clustering plus added parameters to NeighborhooAdd clustering plus added parameters to NeighborhooAdd clustering plus added parameters to Neighborhooddd

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Project
+
+	conda env create -f env.yml
+
+Activate the environment
+	conda activate caa_project_venv
+Deactivate environnement
+	conda deactivate
+    
+Example of running command for Neighborhood
+python ./scripts/Neighborhood.py -f "./data/input_addresses.json" -d 1 -t "data/transactions.json" -nb 4 -s True
+
+Example of running command for clustering
+python ./scripts/clustering.py -o "data/clusters.json" -t "data/transactions.json"
--- scripts/Neighborhood.py
+++ scripts/Neighborhood.py
@@ -6,7 +6,7 @@ import networkx as nx
 from networkx.readwrite import json_graph
 from pyvis.network import Network
 import json
-
+import statistics
 
 class RELATIONSHIP(Enum):
     VOUT = 0
@@ -15,11 +15,14 @@ class RELATIONSHIP(Enum):
 api = BitcoinAPI()
 G = nx.DiGraph()
 
-usedTransactions = list()
 jupyterNotebook = False
 
 def main(args):
+    Network_Transactions = []
+    usedTransactions = list()
+    
     depth = args.depth if args.depth != None else 1;
+    nb = args.nb_adresses if args.nb_adresses != None else None;
     jupyterNotebook = args.jupyterNotebook if args.jupyterNotebook  != None else False
     physics = args.physics if args.physics  != None else True
     net = Network(directed=True,
@@ -28,7 +31,9 @@ def main(args):
                   select_menu=True)
     with open(args.file, 'r') as ft:
         addrPool = json.load(ft)
-
+    
+    if nb !=None:
+        addrPool = addrPool[:min(len(addrPool),nb)]
     iteration = 1
     for address in addrPool: 
         G.add_node(address,color="#ff0000")
@@ -37,8 +42,9 @@ def main(args):
     while(iteration <= depth):
         print("Iteration "+str(iteration)+"/"+str(depth))
         iteration += 1;
-        addrPool = createNeighborhood(addrPool)
-    
+        addrPool, Network_Transactions = createNeighborhood(addrPool)
+        usedTransactions += Network_Transactions
+    usedTransactions = list(set(usedTransactions))
     print("Created a Network with "+str(len(G.nodes))+" Nodes and "+str(len(G.edges))+" Edges")
 
 
@@ -60,7 +66,8 @@ def main(args):
         net.from_nx(G)
         net.show_buttons(filter_=['physics'])
         net.toggle_physics(physics)
-#        net.set_options("""const options = {      
+        
+        #        net.set_options("""const options = {      
 #  "physics": {
 #    "forceAtlas2Based": {
 #      "springLength": 100
@@ -71,6 +78,42 @@ def main(args):
 #}""")
         net.show(args.htmlOutput,notebook=jupyterNotebook)
         print("Done!")
+     
+    if (args.stats != None):
+        # Calculate the in-degree and out-degree distributions
+        in_degrees = dict(G.in_degree())
+        out_degrees = dict(G.out_degree())
+
+        # Calculate the median, minimum, and maximum of the in-degrees
+        in_median = statistics.median(in_degrees.values())
+        in_min = min(in_degrees.values())
+        in_max = max(in_degrees.values())
+
+        # Calculate the median, minimum, and maximum of the out-degrees
+        out_median = statistics.median(out_degrees.values())
+        out_min = min(out_degrees.values())
+        out_max = max(out_degrees.values())
+
+        # Calculate the number of weakly connected components
+        num_components = nx.number_weakly_connected_components(G)
+
+        # Store the results in a dictionary
+        results = {
+            'Number of Weakly Connected Components': num_components,
+            'Number of transactions' : len(usedTransactions),
+            'In-Degree Min': in_min,
+            'In-Degree Median': in_median,
+            'In-Degree Max': in_max,
+            'Out-Degree Min': out_min,
+            'Out-Degree Median': out_median,
+            'Out-Degree Max': out_max
+        }
+
+        # Display the results
+        for key, value in results.items():
+            print(key + ":", value)
+        
+
     
 def draw(G,neighborhood,edgeTitle):
     _vouts = list(neighborhood[RELATIONSHIP.VOUT])
@@ -94,12 +137,14 @@ def cutAddr(addr):
 def createNeighborhood(addrPool):
     #Get transactions for addr
     _newAddresses = set()
+    network_transactions = []
     print("Fetching " +str(len(addrPool))+" addresses")
     for addr in addrPool:
         txs = list(api.getTransactions(addr, args.maxTransactions if args.maxTransactions != None else 25))
         print("Found "+str(len(txs))+" transactions for address " +addr)
         for tx in txs: # for every transaction of addr
             #Get its inputs and outputs
+            network_transactions.append(tx['txid'])
             _vouts = list(map((lambda x:x["scriptpubkey_address"] if "scriptpubkey_address" in x else None),tx["vout"]))
             _vins = list(map(lambda x:x["prevout"]["scriptpubkey_address"],tx["vin"]))
             print("Found "+str(len(_vins))+" inputs and "+str(len(_vouts))+" outputs in transaction "+tx["txid"])
@@ -117,7 +162,7 @@ def createNeighborhood(addrPool):
                         G.nodes[_from]['label'] = cutAddr(_from)
                         G.add_edge(_from,_to)
                         G.edges[_from,_to]['title'] = tx["txid"]
-    return list(_newAddresses)
+    return list(_newAddresses), network_transactions
             
     
     
@@ -128,6 +173,13 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-f', '--file',
                         help='Inputfile with wallet addresses', type=str, required = True)
+    
+    parser.add_argument('-nb', '--nb_adresses',
+                        help='Number of illegal addresses we want to use', type=int, required = False)
+    
+    parser.add_argument('-s', '--stats',
+                        help='Statistiques about the network', type=bool, required = False)
+    
     parser.add_argument('-d', '--depth',
                         help='Neighbors distance (Default 1)', type=int, required = False)
     parser.add_argument('-html', '--htmlOutput',
